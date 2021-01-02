5ca10201fd8316c17a2a24fe8abb2a0fb78e2653
Juan Tello <juanstq20@gmail.com>
2021-01-01 22:01:20 -0500

    Configuracion de React

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom'
+import HelloWorld from './components/HelloWorld'
+import React from 'react'
+const container = document.getElementById('app')
+
+ReactDOM.render(<HelloWorld></HelloWorld>,container);
